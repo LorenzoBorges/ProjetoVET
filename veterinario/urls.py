0722e43316da0e1158bd1a5e40aa3e4eaa8d5e77
lorenzo <lorenzocsborges@gmail.com>
2020-07-27 08:46:40 -0300

    Criado paginação

--- veterinario/urls.py
+++ veterinario/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('clientes.urls')),
-    path('vacinas/', include('clientes.urls')),
+    #    path('vacinas/', include('clientes.urls')),
     path('admin/', admin.site.urls),
 ]
