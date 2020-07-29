69539f213fb66cf3d91a848a899becf65b8567e3
lorenzo <lorenzocsborges@gmail.com>
2020-07-29 04:42:52 -0300

    Commit base para etapa de criação de históricos

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,34 +1,46 @@
 from django.contrib import admin
 from . import models
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
 
-class VacinaInline(admin.TabularInline):
+class PlanoInline(NestedStackedInline):
+    model = models.Plano
+    extra = 1
+    fk_name='level'
+
+class VacinaInline(NestedStackedInline):
     model = models.Vacina
     extra = 1
+    fk_name='level'
 
-class ConsultaInline(admin.TabularInline):
+class ConsultaInline(NestedStackedInline):
     model = models.Consulta
     extra = 1
-
-class CirurgiaInline(admin.TabularInline):
+    fk_name='level'
+    
+class CirurgiaInline(NestedStackedInline):
     model = models.Cirurgia
     extra = 1
+    fk_name='level'
 
-class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'animal', 'raca', 'dono',
-                    'telefone', 'plano', 'mostrar')
-    list_display_links = ('id', 'animal', 'dono')
+
+class AnimalInline(NestedStackedInline):
+    model = models.Animal
+    extra = 1
+    fk_name = 'level'
+    inlines = [VacinaInline, CirurgiaInline, ConsultaInline, PlanoInline]
+
+class ClienteAdmin(NestedModelAdmin):
+    list_display = ('id', 'cliente',
+                    'telefone', 'mostrar')
+    list_display_links = ('id', 'cliente')
     list_per_page = 10
-    search_fields = ('dono', 'animal')
+    search_fields = ('cliente',)
     list_editable = ('telefone', 'mostrar')
-    inlines = [
-        VacinaInline,
-        ConsultaInline,
-        CirurgiaInline,
-    ]
+    inlines = [AnimalInline]
 
 
 admin.site.register(models.Plano)
 admin.site.register(models.Cliente, ClienteAdmin)
-admin.site.register(models.Vacina)
 admin.site.register(models.Consulta)
 admin.site.register(models.Cirurgia)
+admin.site.register(models.Vacina)
--- clientes/models.py
+++ clientes/models.py
@@ -1,60 +1,65 @@
 from django.db import models
 from django.utils import timezone
 
-
-class Plano(models.Model):
-    nome = models.CharField(max_length=255)
+class Cliente(models.Model):
+    cliente = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_registro = models.DateTimeField(verbose_name='Data de registro', default=timezone.now)
+    descricao = models.TextField(verbose_name='Descrição', blank=True)
+    mostrar = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.nome
+        return self.cliente
 
-class Cliente(models.Model):
-    dono = models.CharField(max_length=255)
+class Animal(models.Model):
     animal = models.CharField(max_length=255)
     raca = models.CharField(verbose_name='Raça', max_length=255)
     peso = models.CharField(max_length=255, blank=True)
     especie = models.CharField(verbose_name='Espécie', max_length=255, blank=True)
     idade = models.CharField(max_length=255, blank=True)
-    telefone = models.CharField(max_length=255)
-    email = models.CharField(max_length=255, blank=True)
-    data_criacao = models.DateTimeField(verbose_name='Data de criação', default=timezone.now)
-    descricao = models.TextField(verbose_name='Descrição', blank=True)
-    plano = models.ForeignKey(Plano, on_delete=models.DO_NOTHING)
+    cliente = models.CharField(max_length=255, null=True)
     mostrar = models.BooleanField(default=True)
+    level = models.ForeignKey('Cliente', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.animal
 
-# class Intermediario(models.Model):
-#     dono = models.CharField(max_length=255)
-#     dupla_vacina = models.ManyToManyField(Cliente, through='Vacina')
-
-#     def __str__(self):
-#         return self.dono
-
 class Vacina(models.Model):
-    animal = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    animal = models.CharField(max_length=255)
     vacina = models.CharField(max_length=255)
     data = models.DateField(blank=True)
     descricao_da_vacina = models.TextField(verbose_name='Descrição da vacina', blank=True)
+    level = models.ForeignKey('Animal', on_delete=models.CASCADE, null=True)
+
 
     def __str__(self):
         return self.vacina
 
 class Consulta(models.Model):
-    animal = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    animal = models.CharField(max_length=255)
     consulta = models.CharField(max_length=255)
     data = models.DateField(blank=True)
     descricao_da_consulta = models.TextField(verbose_name='Descrição da consulta', blank=True)
+    level = models.ForeignKey('Animal', on_delete=models.CASCADE, null=True)
+
 
     def __str__(self):
         return self.consulta
 
 class Cirurgia(models.Model):
-    animal = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    animal = models.CharField(max_length=255)
     cirurgia = models.CharField(max_length=255)
     data = models.DateField(blank=True)
     descricao_da_cirurgia = models.TextField(verbose_name='Descrição da cirurgia', blank=True)
+    level = models.ForeignKey('Animal', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.cirurgia
+
+class Plano(models.Model):
+    plano = models.CharField(max_length=255)
+    level = models.ForeignKey('Animal', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.plano
